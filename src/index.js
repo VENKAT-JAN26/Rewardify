2c1428f972a324824f3b7a069fb013b89b84a624
VENKAT-JAN26 <venkatklsr@gmail.com>
2024-11-05 14:23:25 +0530

    QR CODE SCANER READER CORRECTED

--- src/index.js
+++ src/index.js
@@ -7,27 +7,27 @@ import { Provider } from 'react-redux';
 import store from './utils/store';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
-window.addEventListener("load", function() {
-    // Wait for the page to fully load
-    setTimeout(() => {
-        // Select the Syncfusion license message div by style or other identifying properties
-        const licenseMessage = document.querySelector('div[style*="Syncfusion Essential Studio"]');
-        if (licenseMessage) {
-            licenseMessage.remove();
-        }
+// window.addEventListener("load", function() {
+//     // Wait for the page to fully load
+//     setTimeout(() => {
+//         // Select the Syncfusion license message div by style or other identifying properties
+//         const licenseMessage = document.querySelector('div[style*="Syncfusion Essential Studio"]');
+//         if (licenseMessage) {
+//             licenseMessage.remove();
+//         }
 
-        // Remove specific span or anchor tags if they are still there
-        const syncfusionSpan = document.querySelector("span");
-        if (syncfusionSpan) {
-            syncfusionSpan.remove();
-        }
+//         // Remove specific span or anchor tags if they are still there
+//         const syncfusionSpan = document.querySelector("span");
+//         if (syncfusionSpan) {
+//             syncfusionSpan.remove();
+//         }
 
-        const syncfusionLink = document.querySelector('a[href*="claim-license-key"]');
-        if (syncfusionLink) {
-            syncfusionLink.remove();
-        }
-    }, 100); // Adjust the delay if necessary
-});
+//         const syncfusionLink = document.querySelector('a[href*="claim-license-key"]');
+//         if (syncfusionLink) {
+//             syncfusionLink.remove();
+//         }
+//     }, 100); // Adjust the delay if necessary
+// });
 
 
 
